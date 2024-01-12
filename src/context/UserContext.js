2c304e91210db44a753ce41bd1d028e6296cb971
gunjan <gunjan.soral@gmail.com>
2024-01-12 16:30:37 +0530

    login page dropdown

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,53 +1,39 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom/dist";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({});
-    const location = useLocation();
-    const navigate = useNavigate();
+    // const [user, setUser] = useState({
+    //     token: '',
+    //     isLoggedIn: false,
+    // });
+    // const navigate = useNavigate();
 
-    const getTokenFromURL = () => new URLSearchParams(location.search).get('token');
-    const getUserFromURL = () => new URLSearchParams(location.search).get('user');
+    // const storedToken = localStorage.getItem('token') || '';
+    // const storedUser = JSON.parse(localStorage.getItem('user')) || {}; // Parsing stored user data
 
-    const tokenFromURL = getTokenFromURL();
-    const userFromURL = getUserFromURL();
+    // useEffect(() => {
+    //     if (storedToken && storedUser) {
+    //         setUser({
+    //             _id: storedUser?._id,
+    //             name: storedUser?.name,
+    //             email: storedUser?.email,
+    //             token: storedToken,
+    //             isLoggedIn: !!storedToken,
+    //         })
+    //     }
+    // }, [user]);
 
-    const storedToken = localStorage.getItem('token') || '';
-    const storedUser = JSON.parse(JSON.parse(localStorage.getItem('user'))) || {}; // Parsing stored user data
-
-    useEffect(() => {
-        if (storedUser && storedToken) {
-            setUser({
-                _id: storedUser?._id,
-                name: storedUser?.name,
-                email: storedUser?.email,
-                token: storedToken,
-                isLoggedIn: !!storedToken,
-            })
-        }
-        if (tokenFromURL && userFromURL) {
-            localStorage.setItem('token', tokenFromURL);
-            localStorage.setItem('user', JSON.stringify(userFromURL));
-            setUser({
-                _id: userFromURL?._id || "",
-                name: userFromURL?.name || "Avatar",
-                email: userFromURL?.email || "demo.email@domain.com",
-                token: tokenFromURL,
-                isLoggedIn: true,
-            });
-            navigate("/dashboard");
-        }
-    }, [tokenFromURL, userFromURL, navigate]);
-
-    useEffect(() => {
-        if (!user.isLoggedIn) {
-            navigate("/login");
-        }
-    }, [user.isLoggedIn, navigate]);
+    // useEffect(() => {
+    //     if (!user.isLoggedIn) {
+    //         navigate("/login");
+    //     }
+    // }, [user.isLoggedIn, navigate]);
 
     return <UserContext.Provider value={{
-        user, setUser
+        // user, setUser
     }} >
         {children}
     </UserContext.Provider>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,14 +1,69 @@
 import './style.scss'
 import Logo from '../../assets/logo.svg'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { backend_url } from '../../config';
+import axios from 'axios';
+import DropDownMenu from '../../components/dropDownMenu';
 const Login = () => {
-
+  const selectRoleRef = useRef(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [signup, setSignup] = useState(false);
 
-    const handleSignUp = () => {
-      setSignup(true);
-    };
-  
+  const handleLogin = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/login`, {
+        params: {
+          username,
+          password
+        }
+      })
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', data.user);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSignUp = () => {
+    setSignup(true);
+  };
+
+  const [menuItems, setMenuItems] = useState([
+    {
+      item: 'Admin',
+      onClick: () => {
+        console.log('Admin clicked')
+      }
+    },
+    {
+      item: 'TSE',
+      onClick: () => {
+        console.log('TSE clicked')
+      }
+    },
+    {
+      item: 'ASE',
+      onClick: () => {
+        console.log('ASE clicked')
+      }
+    },
+    {
+      item: 'BH',
+      onClick: () => {
+        console.log('BH clicked')
+      }
+    },
+  ])
+
+  const dropDownMenuStyle = {
+    container: {
+      width: '380px',
+
+    }
+  }
+
+
   return (
     <div className='Login-container'>
       <div className="login-main-container">
@@ -16,22 +71,30 @@ const Login = () => {
           <img src={Logo} alt="" />
         </div>
         <div className="login-inside-container">
-          <input type="text" placeholder='User Name' />
+          <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} placeholder='User Name' />
           <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
+          <input type="text" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password" />
           {/* <input type="text" /> */}
           {!signup ? (
-            <select>
-              <option value="" disabled defaultValue>Role</option>
-              <option value="admin">Admin</option>
-              <option value="user">User</option>
-              <option value="guest">Guest</option>
-            </select>
+            <div ref={selectRoleRef} className="login-drop-down">
+              <div className="drop-down-button">
+                Select Role
+              </div>
+              <DropDownMenu
+                menuItems={menuItems}
+                style={{
+                  container: {
+                    // width: '388px',
+                  },
+                }}
+                clickableRef={selectRoleRef}
+              />
+            </div>
           ) : (
             <input type="text" placeholder="Additional Field for Signup" />
           )}
           <div className="buttons">
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
             <button onClick={handleSignUp}>Signup</button>
           </div>
         </div>

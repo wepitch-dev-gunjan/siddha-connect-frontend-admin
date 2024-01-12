aed26bb161d09c478983aef1752a7f2074a069c3
gunjan <gunjan.soral@gmail.com>
2024-01-12 11:25:35 +0530

    hsdfjasdf

--- src/App.js
+++ src/App.js
@@ -1,78 +1,12 @@
+import { Route, Routes } from "react-router-dom/dist";
 import "./style.scss";
-import { useContext } from "react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { AdminContext } from "./context/AdminContext";
-import Profile from "./pages/profile/Profile";
-import Header from "./components/header";
-import Sidebar from "./components/sidebar";
-import Dashboard from "./pages/dashboard";
-import Notifications from "./components/notifications";
-import { NotificationContext } from "./context/NotificationContext";
-import { useRef } from "react";
-import useClickOutside from "./customHooks/useClickOutside";
-import { ToastContainer } from "react-toastify";
-import { ProfileContext } from "./context/ProfileContext";
-import AddProfilePic from "./components/profilePic/addProfilePic";
+import Login from './pages/login';
 
 function App() {
-  const addProfilePicRef = useRef(null);
-  const { admin, setAdmin } = useContext(AdminContext);
-  const { profilePicEditMode, setProfilePicEditMode } =
-    useContext(ProfileContext);
-
-  const { notificationsEnable, setNotificationsEnable, notificationsRef } =
-    useContext(NotificationContext);
-  const { isLoggedIn } = admin;
-
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("admin");
-    setAdmin({ ...admin, isLoggedIn: false });
-    navigate("/login");
-  };
-
-  useClickOutside(addProfilePicRef, () => {
-    setProfilePicEditMode((prev) => !prev);
-  });
-
-  useClickOutside(notificationsRef, () => {
-    setNotificationsEnable(false);
-  });
-
   return (
-    <div>
-      {profilePicEditMode && (
-        <div className="add-profile-pic-panel">
-          <AddProfilePic ref={addProfilePicRef} />
-        </div>
-      )}
-
-      {isLoggedIn && <Header handleLogout={handleLogout} />}
-      <div className="main">
-        <ToastContainer />
-
-        {notificationsEnable && <Notifications />}
-
-        {isLoggedIn && <Sidebar />}
-        <div className={`${isLoggedIn && "main-content"}`}>
-          <Routes>
-            {isLoggedIn ? (
-              <>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/login" element={<Navigate replace to="/" />} />
-              </>
-            ) : (
-              <>
-                <Route path="*" element={<Navigate replace to="/login" />} />
-              </>
-            )}
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { MediaQueryProvider } from "./context/MediaQueryContext";
-import { ProfileProvider } from "./context/ProfileContext";
-import { DashboardProvider } from "./context/DashboardContext";
-import { AdminProvider } from "./context/AdminContext";
-import { NotificationProvider } from "./context/NotificationContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <BrowserRouter>
-        <MediaQueryProvider>
-          <AdminProvider>
-            <ProfileProvider>
-              <DashboardProvider>
-                <NotificationProvider>
-                  <App />
-                </NotificationProvider>
-              </DashboardProvider>
-            </ProfileProvider>
-          </AdminProvider>
-        </MediaQueryProvider>
-      </BrowserRouter>
-    </LocalizationProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );

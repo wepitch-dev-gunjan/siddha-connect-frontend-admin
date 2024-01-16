a4ede6f6f440236f232a00dad887831484872423
gunjan <gunjan.soral@gmail.com>
2024-01-16 16:16:43 +0530

    kis cheez ka h ye kya h?

--- src/components/dropDown/index.jsx
+++ src/components/dropDown/index.jsx
@@ -1,20 +1,35 @@
 // DropDown.jsx
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Dropdown } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 import './style.less';
+import { backend_url } from '../../config'
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const DropDown = () => {
+  const [roles, setRoles] = useState([]);
+
+  const getRoles = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/role`);
+      setRoles(data);
+    } catch (error) {
+      console.log(error);
+      toast('Error fetching roles');
+    }
+  }
+
+  useEffect(() => {
+    getRoles();
+  }, [])
+
   return (
-    <Dropdown title="Dropdown" className="custom-dropdown-container">
-      <Dropdown.Item>New File</Dropdown.Item>
-      <Dropdown.Item>New File with Current Profile</Dropdown.Item>
-      <Dropdown.Item>Download As...</Dropdown.Item>
-      <Dropdown.Item>Export PDF</Dropdown.Item>
-      <Dropdown.Item>Export HTML</Dropdown.Item>
-      <Dropdown.Item>Settings</Dropdown.Item>
-      <Dropdown.Item>About</Dropdown.Item>
+    <Dropdown title="Dropdown" className="DropDown-container" >
+      {roles.map((role, i) => (
+        <Dropdown.Item key={i}>{role.name}</Dropdown.Item>
+      ))}
     </Dropdown>
   );
 };

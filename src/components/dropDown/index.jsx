9da75b9f145563c8db3930e35e51ead7945e324b
gunjan <gunjan.soral@gmail.com>
2024-01-16 15:29:46 +0530

    pusssshhhh

--- src/components/dropDown/index.jsx
+++ src/components/dropDown/index.jsx
@@ -8,19 +8,13 @@ import './style.less';
 const DropDown = () => {
   return (
     <Dropdown title="Dropdown" className="custom-dropdown-container">
-      {/* Customize the Dropdown toggle button */}
-      <Dropdown.Toggle className="custom-dropdown-toggle">Dropdown</Dropdown.Toggle>
-
-      {/* Dropdown items go here */}
-      <Dropdown.Menu>
-        <Dropdown.Item>New File</Dropdown.Item>
-        <Dropdown.Item>New File with Current Profile</Dropdown.Item>
-        <Dropdown.Item>Download As...</Dropdown.Item>
-        <Dropdown.Item>Export PDF</Dropdown.Item>
-        <Dropdown.Item>Export HTML</Dropdown.Item>
-        <Dropdown.Item>Settings</Dropdown.Item>
-        <Dropdown.Item>About</Dropdown.Item>
-      </Dropdown.Menu>
+      <Dropdown.Item>New File</Dropdown.Item>
+      <Dropdown.Item>New File with Current Profile</Dropdown.Item>
+      <Dropdown.Item>Download As...</Dropdown.Item>
+      <Dropdown.Item>Export PDF</Dropdown.Item>
+      <Dropdown.Item>Export HTML</Dropdown.Item>
+      <Dropdown.Item>Settings</Dropdown.Item>
+      <Dropdown.Item>About</Dropdown.Item>
     </Dropdown>
   );
 };

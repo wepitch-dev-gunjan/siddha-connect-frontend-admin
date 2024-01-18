b24a022e97a8b89fc6c125d49bb331746028c822
Nmn200 <n9462895233@gmail.com>
2024-01-18 12:03:59 +0530

    icons

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,11 +1,16 @@
 import 'rsuite/dist/rsuite.min.css';
 import "./style.scss";
 import { Sidenav, Nav } from 'rsuite';
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import { FaChartLine } from "react-icons/fa6";
+import { IoAnalytics } from "react-icons/io5";
+import { MdOutlineFileUpload } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { LuLayoutTemplate } from "react-icons/lu";
+import { LiaToolsSolid } from "react-icons/lia";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdOutlineSecurity } from "react-icons/md";
+import { RiAiGenerate } from "react-icons/ri";
+import { FaUsersViewfinder } from "react-icons/fa6";
 
 const panelStyles = {
   padding: '15px 20px',
@@ -33,15 +38,15 @@ const Sidebar = () => {
               <Nav.Item panel style={panelStyles}>
                 Data Visualization
               </Nav.Item>
-              <Nav.Item eventKey="2-1"  icon={<FaChartLine />}>Charts & Graphs</Nav.Item>
-              <Nav.Item eventKey="2-2">Analytics Dashboard</Nav.Item>
+              <Nav.Item eventKey="2-1"  icon={<FaChartLine />} style={{ display: 'flex', gap: 12 }}>Charts & Graphs</Nav.Item>
+              <Nav.Item eventKey="2-2"  icon={<IoAnalytics />} style={{ display: 'flex', gap: 12 }}>Analytics Dashboard</Nav.Item>
               <Nav.Item divider />
             
             <Nav.Item panel style={panelStyles}>
                 Upload Data
               </Nav.Item>
-              <Nav.Item eventKey="2-1">File Upload</Nav.Item>
-              <Nav.Item eventKey="2-2">Upload History</Nav.Item>
+              <Nav.Item eventKey="2-1" icon={<MdOutlineFileUpload />} style={{ display: 'flex', gap: 12 }}>File Upload</Nav.Item>
+              <Nav.Item eventKey="2-2" icon={<FaHistory />} style={{ display: 'flex', gap: 12 }}>Upload History</Nav.Item>
               <Nav.Item divider />
 
             {/* <Nav.Menu eventKey="3" title="Report Generation" icon={<MagicIcon />}> */}
@@ -49,23 +54,23 @@ const Sidebar = () => {
               <Nav.Item panel style={panelStyles}>
                 Report Generation
               </Nav.Item>
-              <Nav.Item eventKey="3-1">Template Management</Nav.Item>
-              <Nav.Item eventKey="3-2">Customization Tools</Nav.Item>
+              <Nav.Item eventKey="3-1" icon={<LuLayoutTemplate />} style={{ display: 'flex', gap: 12 }}>Template Management</Nav.Item>
+              <Nav.Item eventKey="3-2" icon={<LiaToolsSolid />} style={{ display: 'flex', gap: 12 }}>Customization Tools</Nav.Item>
               <Nav.Item divider />
               {/* </Nav.Menu> */}
 
               <Nav.Item panel style={panelStyles}>
                 User Settings
               </Nav.Item>
-              <Nav.Item eventKey="2-1">Profile</Nav.Item>
-              <Nav.Item eventKey="2-2">Security</Nav.Item>
+              <Nav.Item eventKey="2-1" icon={<FaRegUserCircle />} style={{ display: 'flex', gap: 12 }}>Profile</Nav.Item>
+              <Nav.Item eventKey="2-2" icon={<MdOutlineSecurity />} style={{ display: 'flex', gap: 12 }}>Security</Nav.Item>
               <Nav.Item divider />
 
               <Nav.Item panel style={panelStyles}>
                 Role Management
               </Nav.Item>
-              <Nav.Item eventKey="2-1">Role Generation</Nav.Item>
-              <Nav.Item eventKey="2-2">View All Roles</Nav.Item>
+              <Nav.Item eventKey="2-1" icon={<RiAiGenerate />} style={{ display: 'flex', gap: 12 }}>Role Generation</Nav.Item>
+              <Nav.Item eventKey="2-2" icon={<FaUsersViewfinder />} style={{ display: 'flex', gap: 12 }}>View All Roles</Nav.Item>
               <Nav.Item divider />
 
           </Nav>

3ae0891c21d87f93335520c6ae48e3500be93d99
Nmn200 <n9462895233@gmail.com>
2024-01-23 10:33:21 +0530

    profile

--- src/pages/employee-profile/index.jsx
+++ src/pages/employee-profile/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.scss'
+import { ProfileContext } from '../../context/ProfileContext';
+import BasicInfo from '../../components/basicInfo';
 
 const EmployeeProfile = () => {
+  const { profile, setProfile } = useContext(ProfileContext);
+
   return (
     <div className="EmployeeProfile-container">
         <div className="name">
-            
+            <h2>{profile.name}</h2>
+        </div>
+        <div className="profile-info">
+        <BasicInfo
+              profile={profile}
+              setProfile={setProfile}
+            />
         </div>
     </div>
   )

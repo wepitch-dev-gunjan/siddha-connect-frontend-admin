acca96e767616876f23815971c130037167e2119
Nmn200 <n9462895233@gmail.com>
2024-01-25 11:00:56 +0530

    role

---
+++ src/components/tree/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Tree from 'react-d3-tree';
+
+const myTreeData = [
+  {
+    name: 'ADMIN',
+    children: [
+      {
+        name: 'RSM',
+        children: [
+          { 
+            name: 'ZSE',
+            children: [
+                {
+                    name: 'SEC',
+                    children: [
+                        {
+                            name: 'ZSM',
+                            children: [
+                                {
+                                    name: 'RSO',
+                                    children: [
+                                        {
+                                            name: 'ASE',
+                                            children: [
+                                                {
+                                                    name: 'ABM',
+                                                    children: [
+                                                        {
+                                                            name: 'BSM',
+                                                            children: [
+                                                                {
+                                                                    name: 'ABM',
+                                                                    children: [
+                                                                        {
+                                                                            name: 'TSE'
+                                                                        }
+                                                                    ]
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        
+        },
+          
+        ],
+      },
+    ],
+  },
+];
+
+const HierarchyChart = () => {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <Tree data={myTreeData} orientation="vertical" />
+    </div>
+  );
+};
+
+export default HierarchyChart;
--- src/pages/role/index.jsx
+++ src/pages/role/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './style.scss'
+import HierarchyTree from '../../components/tree'
 
 const Role = () => {
   return (
     <>
-    char
+    <HierarchyTree />
     </>
   )
 }

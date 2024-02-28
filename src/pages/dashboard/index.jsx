fb8b249a5b5d9fa807ee100f67357552e3280ca7
Nmn200 <n9462895233@gmail.com>
2024-02-28 17:16:32 +0530

    fsd

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,70 +5,74 @@ import { CounsellorData, UserData, EarningsData, ActivityData } from '../../comp
 import LineChart from "../../components/dashboardComponents/lineChart";
 import GroupedBarChart from "../../components/dashboardComponents/groupedBarChart";
 import RecentActivityTable from "../../components/recentActivityTable";
+import SalesDashboard from "../../components/salesDashboard";
 
 const Dashboard = () => {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((el) => el.month),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map(el => el.userGain),
-      },
-      {
-        label: "Users Lost",
-        data: UserData.map(el => el.userLost),
-      },
+  // const [userData, setUserData] = useState({
+  //   labels: UserData.map((el) => el.month),
+  //   datasets: [
+  //     {
+  //       label: "Users Gained",
+  //       data: UserData.map(el => el.userGain),
+  //     },
+  //     {
+  //       label: "Users Lost",
+  //       data: UserData.map(el => el.userLost),
+  //     },
 
-    ]
-  })
-  const [counsellorData, setCounsellorData] = useState({
-    labels: CounsellorData.map((el) => el.month),
-    datasets: [
-      {
-        label: "Counsellors Gained",
-        data: CounsellorData.map(el => el.counsellorGain),
-      },
-      {
-        label: "Counsellors Lost",
-        data: CounsellorData.map(el => el.counsellorLost),
-      },
+  //   ]
+  // })
+  // const [counsellorData, setCounsellorData] = useState({
+  //   labels: CounsellorData.map((el) => el.month),
+  //   datasets: [
+  //     {
+  //       label: "Counsellors Gained",
+  //       data: CounsellorData.map(el => el.counsellorGain),
+  //     },
+  //     {
+  //       label: "Counsellors Lost",
+  //       data: CounsellorData.map(el => el.counsellorLost),
+  //     },
 
-    ]
-  })
-  const [earningsData, setEarningsData] = useState({
-    labels: EarningsData.map((el) => el.month),
-    datasets: [
-      {
-        label: "Earnings Gained",
-        data: EarningsData.map(el => el.earning),
-      },
-    ]
-  })
-  const [activityData, setActivityData] = useState({
-    labels: ActivityData.map((el) => el.day),
-    datasets: [
-      {
-        label: "Online Users",
-        data: ActivityData.map(el => el.onlineUsers),
-      },
-      {
-        label: "Online Counsellors",
-        data: ActivityData.map(el => el.onlineCounsellors),
-      },
-    ]
-  })
+  //   ]
+  // })
+  // const [earningsData, setEarningsData] = useState({
+  //   labels: EarningsData.map((el) => el.month),
+  //   datasets: [
+  //     {
+  //       label: "Earnings Gained",
+  //       data: EarningsData.map(el => el.earning),
+  //     },
+  //   ]
+  // })
+  // const [activityData, setActivityData] = useState({
+  //   labels: ActivityData.map((el) => el.day),
+  //   datasets: [
+  //     {
+  //       label: "Online Users",
+  //       data: ActivityData.map(el => el.onlineUsers),
+  //     },
+  //     {
+  //       label: "Online Counsellors",
+  //       data: ActivityData.map(el => el.onlineCounsellors),
+  //     },
+  //   ]
+  // })
   return (
     <div className="all-dashboard">
       <div className="Dashboard-container">
         <div className="business-dashbaord">
-          <h1>CHARTS & GRAPHS</h1>
-          <div className="widgets-container">
+          {/* <h1>Sales Dashboard</h1> */}
+
+          <SalesDashboard />
+
+          {/* <div className="widgets-container">
             <Widget heading="TOTAL SALES" value="$10000" />
             <Widget heading="TOP PRODUCTS" value="1000" />
             <Widget heading="TOP REGIONS" value="1000" />
             <Widget heading="AVERAGE SALES" value="$5000" />
-          </div>
-          <div className="chart-row">
+          </div> */}
+          {/* <div className="chart-row">
             <div className="col">
               <GroupedBarChart data={activityData} />
             </div>
@@ -78,15 +82,15 @@ const Dashboard = () => {
             <div className="col">
               <LineChart data={earningsData} />
             </div>
-          </div>
+          </div> */}
         </div>
 
         {/* recent payments */}
         {/* <RecentPayments /> */}
-        <div className="recent-activity">
+        {/* <div className="recent-activity">
           <h1>Recent Activities</h1>
           <RecentActivityTable />
-        </div>
+        </div> */}
       </div>
     </div>
   );

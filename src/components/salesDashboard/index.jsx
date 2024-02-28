81541e2063decbdf306b36246eac3d66854faa23
Nmn200 <n9462895233@gmail.com>
2024-02-28 17:16:32 +0530

    fsd

---
+++ src/components/salesDashboard/index.jsx
@@ -0,0 +1,91 @@
+// UploadExcel.js
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { backend_url } from '../../config';
+import './style.scss'
+import toast from 'react-hot-toast';
+import { UserContext } from '../../context/UserContext';
+import { TextField } from '@mui/material';
+
+const SalesDashboard = () => {
+  const [data, setData] = useState([]);
+  const [headers, setHeaders] = useState([]);
+const { user } = useContext(UserContext)
+const [searchText, setSearchText] = useState("")
+
+  const getSalesData = async (searchText) => {
+    try {
+        const { data } = await axios.get(`${backend_url}/upload`, {
+            headers: {
+                Authorization: user.token
+            },
+            params: {
+                search: searchText
+            }
+        })
+        console.log(data)
+        setData(data)
+        setHeaders(Object.keys(data[0] || {}).map((header) => ({ key: header, name: header })));
+    } catch (error) {
+        console.log(error.message)
+        toast.error(error.message, "error");
+    }
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      // Handle Enter key press here
+      console.log('Enter key pressed');
+      getSalesData(searchText)
+    }
+  };
+  useEffect(() => {
+    // axios.get(`${backend_url}/upload`)
+    //   .then(response => {
+    //     setData(response.data.data);
+    //     setHeaders(Object.keys(response.data.data[0] || {}).map((header) => ({ key: header, name: header })));
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching data: ', error);
+    //   });
+    getSalesData()
+  }, [user]);
+
+  return (
+    <div className='SalesDashboard-container'>
+        <h1>Sales Dashboard</h1>
+
+        <div className="filter-container">
+            <h2>Filters</h2>
+            <TextField 
+            sx={{ width: 800 }}
+            label="Search input"
+            InputProps={{
+              type: 'search',
+            }}
+            onChange={(e) => {
+                setSearchText(e.target.value)
+            }}
+            value={searchText}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      <div className='sales-table' >
+          <div className='row table-headings'>
+            {headers.map((header, index) => (
+              <div className='col' key={index}>{header.name}</div>
+            ))}
+          </div>
+          {data.map((row, rowIndex) => (
+            <div key={rowIndex} className='row'>
+              {Object.values(row).map((cell, cellIndex) => (
+                <div key={cellIndex} className='col'>{cell}</div>
+              ))}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SalesDashboard;
